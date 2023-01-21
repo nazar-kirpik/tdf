1b2dd16dd38d0bfd0fc0a5475fd19b5d7dc70790
nazar-kirpik <nazarkirpik@gmail.com>
2023-01-22 00:28:30 +0100

    first commit

---
+++ Assets/Scripts/MoneyUI.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class MoneyUI : MonoBehaviour
+{
+
+    public Text moneyText;
+
+    // Update is called once per frame
+    void Update()
+    {
+        moneyText.text = "$" + PlayerStats.Money.ToString();
+    }
+}
---
+++ Assets/Scripts/Shop.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+
+public class Shop : MonoBehaviour
+{
+
+    public TurretBlueprint standartTurret;
+    public TurretBlueprint missileLauncher;
+    public TurretBlueprint laserBeamer;
+
+    BuildManager buildManager;
+
+    void Start ()
+    {
+        buildManager = BuildManager.instance;
+    }
+
+    public void SelectStandartTurret ()
+    {
+        Debug.Log("Standart Turret Selected");
+        buildManager.SelectTurretToBuild(standartTurret);
+    }
+
+    public void SelectMissileLauncher ()
+    {
+        Debug.Log("Missile Launcher Selected");
+        buildManager.SelectTurretToBuild(missileLauncher);
+    }
+
+    public void SelectLaserBeamer ()
+    {
+        Debug.Log("Laser Beamer Selected");
+        buildManager.SelectTurretToBuild(laserBeamer);
+    }
+
+}
